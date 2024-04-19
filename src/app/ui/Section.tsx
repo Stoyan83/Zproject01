25b0b9c2f653b8e70399edd19c2bf435581cf154
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-19 19:10:35 +0300

    Adding the text

--- src/app/ui/Section.tsx
+++ src/app/ui/Section.tsx
@@ -1,43 +1,55 @@
 const Section = () => {
-    return (
-      <div className="flex flex-col items-center">
-        <div className="lg:max-w-[860px]">
-          <h2 className="text-4xl lg:text-4xl lg:w-2/4 p-5 lg:p-10 font-medium text-dark-blue lg:text-left">
-            Решения
-          </h2>
-          <div className="flex flex-row lg:flex-col lg:w-9/12 lg:text-left">
-            <p className="text-dark-blue lg:w-full lg:ml-10 lg:mr-5">
-              Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки местни предприятия до мултинационални компании. <br /> <br /> Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.
-            </p>
-            <ul className="text-dark-blue lg:max-w-[680px] list-disc lg:w-screen lg:ml-[2vw] lg:mr-[10vw] p-5 lg:p-10 lg:text-left">
-              <li>
-                Решения за всички видове стоки (конвенционални, изискващи температурен режим, опасни товари)
-              </li>
-              <li>
-                Превоз на стоки от и до Балканите и всички страни на ЕС, цели и частични товари;
-              </li>
-              <li>Взимане на стоки и доставки за от и в България;</li>
-              <li>
-                Съчетаване на дълги и къси дестинации за ефективно планиране и разходи;
-              </li>
-              <li>Регулярни групажни линии;</li>
-              <li>Транспортни услуги до Турция и Близкия изток;</li>
-              <li>
-                Спазване на изискванията за качество и стандарти на клиентите и/или на техни специфични изисквания.
-              </li>
-            </ul>
-          </div>
-          <h2 className="text-4xl lg:text-4xl lg:w-2/4 p-5 lg:p-10 font-medium text-dark-blue lg:text-left">
-            Предимства
-          </h2>
-          <div className="flex flex-row lg:flex-col lg:max-w-[680px] lg:text-left">
-            <p className="text-dark-blue lg:w-full lg:ml-10 lg:mr-10">
-              Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки местни предприятия до мултинационални <br />  компании. <br /> <br /> Ние обработваме вашата пратка като всяка друга голяма товарна пратка – <br /> старателно и професионално.
-            </p>
-          </div>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="lg:max-w-[860px]">
+        <h2 className="text-4xl lg:text-4xl lg:w-2/4 p-5 lg:p-10 font-medium text-customBlue lg:text-left">
+          Решения
+        </h2>
+        <div className="flex flex-row lg:flex-col lg:w-9/12 lg:text-left">
+          <p className="text-customBlue lg:w-full lg:ml-10 lg:mr-5">
+            Фокусът ни върху клиентите е същността на нашата работа. Ние
+            доставяме услуги за всички видове бизнес – от малки местни
+            предприятия до мултинационални компании. <br /> <br /> Ние
+            обработваме вашата пратка като всяка друга голяма товарна пратка –
+            старателно и професионално.
+          </p>
+          <ul className="text-customBlue lg:max-w-[680px] list-disc lg:w-screen lg:ml-[2vw] lg:mr-[10vw] p-5 lg:p-10 lg:text-left">
+            <li>
+              Решения за всички видове стоки (конвенционални, изискващи
+              температурен режим, опасни товари)
+            </li>
+            <li>
+              Превоз на стоки от и до Балканите и всички страни на ЕС, цели и
+              частични товари;
+            </li>
+            <li>Взимане на стоки и доставки за от и в България;</li>
+            <li>
+              Съчетаване на дълги и къси дестинации за ефективно планиране и
+              разходи;
+            </li>
+            <li>Регулярни групажни линии;</li>
+            <li>Транспортни услуги до Турция и Близкия изток;</li>
+            <li>
+              Спазване на изискванията за качество и стандарти на клиентите
+              и/или на техни специфични изисквания.
+            </li>
+          </ul>
+        </div>
+        <h2 className="text-4xl lg:text-4xl lg:w-2/4 p-5 lg:p-10 font-medium text-customBlue lg:text-left">
+          Предимства
+        </h2>
+        <div className="flex flex-row lg:flex-col lg:max-w-[680px] lg:text-left">
+          <p className="text-customBlue lg:w-full lg:ml-10 lg:mr-10">
+            Фокусът ни върху клиентите е същността на нашата работа. Ние
+            доставяме услуги за всички видове бизнес – от малки местни
+            предприятия до мултинационални <br /> компании. <br /> <br /> Ние
+            обработваме вашата пратка като всяка друга голяма товарна пратка –{" "}
+            <br /> старателно и професионално.
+          </p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Section;
+export default Section;
