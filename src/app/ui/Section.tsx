7ce5c1e281655e3b5c37b9d6a8a8ce844eaa50e9
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-19 18:57:30 +0300

    Adding the text

--- src/app/ui/Section.tsx
+++ src/app/ui/Section.tsx
@@ -1,15 +1,15 @@
 const Section = () => {
     return (
       <div className="flex flex-col items-center">
-        <div className="lg:max-w-[860px]">
+        <div className="lg:max-w-[770px]">
           <h2 className="text-4xl lg:text-4xl lg:w-2/4 p-5 lg:p-10 font-medium text-dark-blue lg:text-left">
             Решения
           </h2>
-          <div className="flex flex-row lg:flex-col lg:w-8/12 lg:text-left">
+          <div className="flex flex-row lg:flex-col lg:w-9/12 lg:text-left">
             <p className="text-dark-blue lg:w-full lg:ml-10 lg:mr-5">
               Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки местни предприятия до мултинационални компании. <br /> <br /> Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.
             </p>
-            <ul className="text-dark-blue list-disc lg:w-full lg:ml-[2vw] lg:mr-[10vw] p-5 lg:p-10 lg:text-left">
+            <ul className="text-dark-blue lg:max-w-[680px] list-disc lg:w-screen lg:ml-[2vw] lg:mr-[10vw] p-5 lg:p-10 lg:text-left">
               <li>
                 Решения за всички видове стоки (конвенционални, изискващи температурен режим, опасни товари)
               </li>
