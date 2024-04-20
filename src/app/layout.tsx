cb1d9798a6127bb92a1a8cffa4572051464234cf
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-20 09:23:47 +0300

    Refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-w-[300px]`} >{children}</body>
+      <body className={`${inter.className} min-w-[300px] bg-white`} >{children}</body>
     </html>
   );
 }
