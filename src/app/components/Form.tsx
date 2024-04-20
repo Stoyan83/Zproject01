52556151be9c9bf6ad2e199b75e48dc7e90a0305
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-20 10:57:39 +0300

    Refactoring

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import EmailIcon from "../ui/icons/EmailIcon";
 
 export default function Form() {
   return (
@@ -13,17 +14,7 @@ export default function Form() {
       </p>
       <div className="my-16 flex flex-nowrap justify-center">
         <div className="flex items-center bg-white rounded-l-xl pl-3 xl:pl-5">
-          <svg
-            className="h-6 w-6 fill-current text-gray-400"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M3 3a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3zm16.828 2H4.172L12 11.896 19.828 5zM4 19V8.104l7.096 6.398a.5.5 0 0 0 .608 0L20 8.104V19H4z"
-            />
-          </svg>
+            <EmailIcon />
           <input
             className="p-3 w-[12rem] xl:w-auto xl:p-5 xl:text-xl outline-none border-none bg-transparent"
             type="text"
---
+++ src/app/ui/icons/EmailIcon.tsx
@@ -0,0 +1,15 @@
+const EmailIcon = () => (
+  <svg
+    className="h-6 w-6 fill-current text-gray-400"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 3a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3zm16.828 2H4.172L12 11.896 19.828 5zM4 19V8.104l7.096 6.398a.5.5 0 0 0 .608 0L20 8.104V19H4z"
+    />
+  </svg>
+);
+
+export default EmailIcon;
